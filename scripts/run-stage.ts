16543424d1301e192cd071058b60472d64894ee0
Rodrigo Santone <rodrigo.santone@scrumlaunch.com>
2026-03-20 17:32:09 -0300

    ts

--- scripts/run-stage.ts
+++ scripts/run-stage.ts
@@ -3,12 +3,9 @@ const { execSync } = require('child_process');
 process.env.ENVIRONMENT = 'stage';
 
 try {
-  execSync(
-    'npx playwright test AR-003-AirRequest-Step2-Scenarios9-10-11-12-13-14-15-16-17.spec.ts --project=chromium',
-    {
-      stdio: 'inherit',
-    },
-  );
+  execSync('npx playwright test  --project=chromium', {
+    stdio: 'inherit',
+  });
 } catch (err) {
   // Do NOT print giant stack trace
   console.error('\n❌ Playwright reported a failure.');
